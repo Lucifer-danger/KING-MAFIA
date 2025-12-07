e8c90a5d9af956aff7e18290a40d8fe5c3f3ea94
ɪᴍᴇꜱʜ ꜱᴀɴᴅᴇᴇᴘᴀ <sandeepaimesh470@gmail.com>
2025-12-07 16:21:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "rkZnUAxR#nUq2gZ3uXH2XRWur1dNzqVSQFs5oeskZzcHuVdzTGVU",
+SESSION_ID: process.env.SESSION_ID || "nhoEkIhC#9MqD_vYIbnQvdF4ZJyHMtDAXXC0kOj1iV4Opwayi_9w",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 KING_MAFIA-MD Is Alive Now😍*",
 BOT_OWNER: '94762183275',  // Replace with the owner's phone number
