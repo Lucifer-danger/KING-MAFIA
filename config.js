779b8519dc6738455deee6b417e8fb397dd2108f
ɪᴍᴇꜱʜ ꜱᴀɴᴅᴇᴇᴘᴀ <sandeepaimesh470@gmail.com>
2025-12-08 13:37:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "nhoEkIhC#9MqD_vYIbnQvdF4ZJyHMtDAXXC0kOj1iV4Opwayi_9w",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Lucifer-danger/KING-MAFIA/blob/main/images/IMG-20251207-WA0004.jpg?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 KING_MAFIA-MD Is Alive Now😍*",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 KING_MAFIA-MD Is Alive Now*\n\n*Version:* ${settings.version} \n*Status:* Online\n*Mode:* Public\n\n*🌟 Features:*\n• Group management\n• Antilink Protection\n• Fun Commands\n• And more!\n\nType *.menu* for full command list",
 BOT_OWNER: '94762183275',  // Replace with the owner's phone number
 
 
